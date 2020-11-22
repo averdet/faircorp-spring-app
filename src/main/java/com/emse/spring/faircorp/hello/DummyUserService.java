fc863c1f543cdbcad191261c215e77dbe98dda0a
Alexandre <alexandre.verdet@etu.emse.fr>
2020-11-22 16:19:46 +0100

    use ArrayList nameList in DummyUserService

--- src/main/java/com/emse/spring/faircorp/hello/DummyUserService.java
+++ src/main/java/com/emse/spring/faircorp/hello/DummyUserService.java
@@ -3,17 +3,27 @@ package com.emse.spring.faircorp.hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class DummyUserService implements UserService{
 
-    private ConsoleGreetingService greeter;
+    private final ConsoleGreetingService greeter;
 
     @Autowired
     public DummyUserService(ConsoleGreetingService greeter){
         this.greeter = greeter;
     }
     public void greetAll() {
-        greeter.greet("Elodie");
-        greeter.greet("Charles");
+        List<String> nameList = new ArrayList<String>();
+        nameList.add("Elodie");
+        nameList.add("Charles");
+
+        for (int i = 0; i < nameList.size(); i++) {
+            greeter.greet(nameList.get(i));
+        }
+        //greeter.greet("Elodie");
+        //greeter.greet("Charles");
     }
 }
