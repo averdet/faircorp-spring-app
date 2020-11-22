6c116e8d12b4c352dd54c4a12c0353fbda55ccfc
Alexandre <alexandre.verdet@etu.emse.fr>
2020-11-22 16:52:19 +0100

    AnotherConsoleGreetingService which says Bonjour, to try @Primary

--- src/main/java/com/emse/spring/faircorp/hello/AnotherConsoleGreetingService.java
+++ src/main/java/com/emse/spring/faircorp/hello/AnotherConsoleGreetingService.java
@@ -4,9 +4,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
-@Primary
-public class ConsoleGreetingService implements GreetingService{
+public class AnotherConsoleGreetingService implements GreetingService{
     public void greet(String name) {
-        System.out.println("Hello, " + name+"!");
+        System.out.println("Bonjour, " + name+"!");
     }
 }
