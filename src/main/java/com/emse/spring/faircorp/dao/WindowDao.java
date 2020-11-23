1f832ce7acd5d3ee75c6c445fffe2a96f2e6abf4
Alexandre <alexandre.verdet@etu.emse.fr>
2020-11-23 12:11:11 +0100

    removed useless

--- src/main/java/com/emse/spring/faircorp/dao/WindowDao.java
+++ src/main/java/com/emse/spring/faircorp/dao/WindowDao.java
@@ -4,5 +4,4 @@ import com.emse.spring.faircorp.model.Window;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WindowDao extends JpaRepository<Window, Long> {
-    Window getOne(Long id);
 }
